32315a4116f9cc4ad9c82bcb23910cef0f34df84
Ali Kawar <alikawaar8@gmail.com>
2026-01-07 22:15:51 +0200

    updating

--- src/rna_app.py
+++ src/rna_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle
+import joblib
 import re
 
 # Page configuration
@@ -15,10 +15,9 @@ st.set_page_config(
 @st.cache_resource
 def load_model():
     try:
-        with open(r'models/rna_model.joblib', 'rb') as f:
-            model = pickle.load(f)
-        with open(r'models/rna_scaler.joblib', 'rb') as f:
-            scaler = pickle.load(f)
+       
+        model = joblib.load('models/rna_model.joblib')
+        scaler = joblib.load('models/rna_scaler.joblib')
         return model, scaler
     except FileNotFoundError:
         return None, None
